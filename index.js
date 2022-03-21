fa427d701ef779c9c36933757ef08e980f34aa0f
a-nst <nastya.andrianova01@yandex.ru>
2022-03-21 15:36:02 +0300

    a

--- index.js
+++ index.js
@@ -1,27 +1,19 @@
-const express = require("express");
+const express = require('express')
+const exphbs = require('express-handlebars');
+const router = require('./routes/route');
+const PORT = process.env.PORT || 3000
+
 const app = express();
-const AuthController = require("./controllers/AuthController.js");
-
-app.set("view engine", "hbs");
-app.use(express.urlencoded({ extended: false }));
- 
-// определяем роутеры
-const userRouter = express.Router();  // для адресов с "/users"
- 
-userRouter.post("/loginPost", AuthController.login);
-userRouter.get("/login",AuthController.getLoginPage);
-// сопоcтавляем роутер с конечной точкой "/users"
-app.use("/", userRouter);
- 
-// общие обработчики
-
-app.get("/", function (request, response) {
-    response.redirect('/login')
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    extname : 'hbs'
 });
- 
-// обработка ошибки 404
-app.use(function (req, res, next) {
-    res.status(404).send("Not Found")
-});
- 
-app.listen(3000);
+
+app.engine('hbs', hbs.engine)
+app.set('view engine','hbs')
+app.set('views','views')
+
+app.use(router)
+
+
+app.listen(PORT, () => {console.log('Server hes been started...')})
